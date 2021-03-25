e2af741a98a0435d62291f612610276150289344
Dane Laban <Siaki24@gmail.com>
2021-03-24 22:31:20 -0700

    NOTES

---
+++ Console-Log/notes.js
@@ -0,0 +1,25 @@
+// alert('Hello World'); // Makes the webpage say "Hello World" 
+// Log to console
+console.log ('Hello World');
+console.log (123);
+console.log (true);
+var greeting = 'Hello'; 
+console.log (greeting); // logs the greeting from above
+console.log ([1,2,3,4]); // log an ARRAY []
+console.log ({a:1, b:2}); // log an object {}
+console.table({a:1, b:2}); // log a table {index and value}
+
+console.error ('This is an error I wrote'); // log an error
+
+// console.clear(); THIS CLEARS THE CONSOLE...
+
+console.warn('This is my warning'); //log a warning
+console.time('Hello');
+    console.log ('Hello World');
+    console.log ('Hello World');
+    console.log ('Hello World');
+    console.log ('Hello World');
+    console.log ('Hello World');
+    console.log ('Hello World');
+console.timeEnd ('Hello'); // This logs the time it took from console.time to console.timeEnd
+
